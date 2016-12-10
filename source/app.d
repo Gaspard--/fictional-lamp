a8da591246cc46372b82a60bca4dae360269569b
barre_k <kevin.barre@epitech.eu>
2016-12-10 16:48:41 +0100

    glclear

--- source/app.d
+++ source/app.d
@@ -1,10 +1,35 @@
 import std.stdio;
 import core.sys.windows.windows;
 import derelict.glfw3.glfw3;
-import derelict.glfw3;
 
-void main()
+
+int main()
 {
+
 	DerelictGLFW3.load();
-	writefln("haaaaaaaaaaaaa");
+
+
+	if (!glfwInit()) {
+		writeln("glfwInit didn't work");
+		return -1;
+	}
+
+	auto window = glfwCreateWindow(640, 480, "Hello World", null, null);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);		
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
+	writefln("ok");
+	return 0;
 }
